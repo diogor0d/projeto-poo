51d237e552eaa71c3b477082518d6a843ab4775c
Diogo Rodrigues <frodrigues.diogo@gmail.com>
2024-11-20 19:51:51 +0000

    Modelos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        receberLinha();
+
+        ArrayList<Produto> produtos = new ArrayList<>(Arrays.asList(
+                new ProdutoAlimentarTI(1234, "vinho de pacote", "750ml vinho de pacote biologico do douro", 2, 250.00, true, CategoriaAlimentar.VINHO),
+                new ProdutoAlimentarTR(12366, "melao", "500g melao do panelas", 1, 5.00, true, new ArrayList<>(Arrays.asList(Certificacao.HACCP))),
+                new ProdutoAlimentarTN(27311, "queijo", "750g queijo de cabra", 4, 10.00, false)
+        ));
+
+        Cliente cristinoRondo = new Cliente("Cristino Rondo", 23345123, "Madeira");
+        Data dataFaturaExemplo = new Data(23,1,2065);
+
+        Fatura debug = new Fatura(8347113, cristinoRondo, dataFaturaExemplo, produtos);
     }
 
     private void novoCliente(){}
--- src/ProdutoFarmaciaPrescrito.java
+++ src/ProdutoFarmaciaPrescrito.java
@@ -3,7 +3,7 @@ public class ProdutoFarmaciaPrescrito extends Produto {
     private String medico;
     protected double[] taxas = {6,5,4};
 
-    public ProdutoFarmaciaPrescrito(int codigo, String nome, String descricao, int quantidade, double preco, double[] taxas, String medico) {
+    public ProdutoFarmaciaPrescrito(int codigo, String nome, String descricao, int quantidade, double preco, String medico) {
         super(codigo, nome, descricao, quantidade, preco);
         this.taxas = taxas;
         this.medico = medico;
